24e993877685160d6e0d7f5fdb5ccc05c045ef1f
Santakoska <kamalsankarm@gmail.com>
2024-05-04 15:15:44 +0530

    #

---
+++ Boats_to_Save_People.java
@@ -0,0 +1,43 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Boats_to_Save_People {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int a = sc.nextInt();
+        int[] people = new int[a];
+        for(int i=0; i<a; i++)
+        {
+            System.out.println("Enter the number");
+            people[i] = sc.nextInt();
+        }
+        System.out.println("Enter the limit");
+        int limit = sc.nextInt();
+        Solution7 s7 = new Solution7();
+        int ko = s7.numRescueBoats(people, limit);
+        System.out.println(ko);
+    }
+}
+class Solution7 {
+    public int numRescueBoats(int[] people, int limit) {
+        int boat=0;
+        Arrays.sort(people);
+        int heavy = people.length-1;
+        int light = 0;
+
+        while(light <= heavy){
+            if((people[light] + people[heavy]) <= limit){
+                light ++;
+                heavy--;
+                boat++;
+            }
+            else{
+                heavy--;
+                boat++;
+            }
+        }
+        return boat;
+    }
+}
--- Compare_Version_Numbers.java
+++ Compare_Version_Numbers.java
@@ -6,14 +6,30 @@ public class Compare_Version_Numbers {
         String a = sc.next();
         System.out.println("Enter the next version");
         String b = sc.next();
+        Solution5 s5 = new Solution5();
+        int g = s5.compareVersion(a, b);
+        System.out.println(g);
     }
 }
 class Solution5 {
     public int compareVersion(String version1, String version2) {
-        StringBuilder sb = new StringBuilder();
-        int i=0;
-        while(i<version1.length()){
-            
+        int n = version1.length(), m = version2.length();
+        int i = 0, j = 0;
+        while (i < n || j < m) {
+            int x = 0;
+            for (; i < n && version1.charAt(i) != '.'; ++i) {
+                x = x * 10 + version1.charAt(i) - '0';
+            }
+            ++i; 
+            int y = 0;
+            for (; j < m && version2.charAt(j) != '.'; ++j) {
+                y = y * 10 + version2.charAt(j) - '0';
+            }
+            ++j;
+            if (x != y) {
+                return x > y ? 1 : -1;
+            }
         }
+        return 0;
     }
 }
