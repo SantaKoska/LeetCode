24b2a94996e3a23d52758dd2964ca90b9d39f654
Santakoska <kamalsankarm@gmail.com>
2024-05-05 22:14:22 +0530

    #

---
+++ Delete_Node_in_a_Linked_List.java
@@ -0,0 +1,6 @@
+import java.util.LinkedList;
+public class Delete_Node_in_a_Linked_List {
+    public static void main(String[] args) {
+        LinkedList<int> ListNode = new LinkedList<>() 
+    }
+}
